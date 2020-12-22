f990e3d5a8bb4146bb1af584bd3faa890946845d
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-12-22 12:28:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -37,6 +37,21 @@ function returnAjax() {
    }).done(function(data) { return data; });
 }
 
+// Returns promise, will need to make the main while-loop async as well
+// Do this in order for api call to finish before next while loop
+function sendTweets(final_tweet) {
+  return new Promise ((resolve, reject) => {
+    client.post('statuses/update', final_tweet, function(error, tweet, response) {
+      if (error) {
+        reject(error);
+        console.log("\x1b[31mError - Did not send: \x1b[0m" + final_tweet.status)  // Red text
+      }
+      resolve(response);
+      console.log("\x1b[32mSuccessfully sent: \x1b[0m" + final_tweet.status)  // Green text
+    });
+  });
+}
+
 // create AM/PM time return function
 function timeConverter(time) {
   var newTime = time.slice(11,16);
@@ -64,95 +79,89 @@ var visitedReports = []; // store already posted reports, do this to prevent dup
 
 var trackNumber = 0; // Used to number logs
 
-// keep running indefinitely, server style
-while (1) {
-  // 17 is the magic number here, Ajax full report, want 17th item anyways
-  var data = returnAjax(); // pull NYPD SOCRATA API Report json
+(async () => {
+  // keep running indefinitely, server style
+  while (1) {
+    // 17 is the magic number here, Ajax full report, want 17th item anyways
+    var data = returnAjax(); // pull NYPD SOCRATA API Report json
 
-  // Create yesterday's date (API is not live and must be lagged a day behind)
-  // single digit months automatically padded with a 0
-  var today = new Date((new Date()).valueOf() - 1000*60*60*24*2);
-  var today_date = today.toISOString().substring(0, 10);
-  today_date = today_date.slice(0,4) + "-" + today_date.slice(5,7) + "-" + today_date.slice(8,10); // yyyy-mm-dd (NYPD API date format)
-  // match formatting of API dates, create today_date so we can still use time
+    // Create 2 days ago's date (API is not live and must be lagged a 2 days behind)
+    // single digit months automatically padded with a 0
+    var today = new Date((new Date()).valueOf() - 1000*60*60*24*2);
+    var today_date = today.toISOString().substring(0, 10);
+    today_date = today_date.slice(0,4) + "-" + today_date.slice(5,7) + "-" + today_date.slice(8,10); // yyyy-mm-dd (NYPD API date format)
+    // match formatting of API dates, create today_date so we can still use time
 
-  // following code block used to prevent dupes in using a visited queue
-  // once 12:00AM comes around, queue removes data points from the day before and starts fresh
-  var infoStore = []; // store current reports
+    // following code block used to prevent dupes in using a visited queue
+    // once 12:00AM comes around, queue removes data points from the day before and starts fresh
+    var infoStore = []; // store current reports
 
-  // need to index info before adding to infoStore
-  var beforeStore = [];
+    // need to index info before adding to infoStore
+    var beforeStore = [];
 
-  // index beforeStore
-  for (var key of Object.keys(data)) {
-      beforeStore.push(data[key]);  // push reports to info store
-  }
+    // index beforeStore
+    for (var key of Object.keys(data)) {
+        beforeStore.push(data[key]);  // push reports to info store
+    }
 
-  var currentTime = '';  // init current time
-  currentTime = currentTime.concat(String(today.getHours()), String(today.getMinutes()), String(today.getSeconds())); // hours:minutes:seconds to string
+    var currentTime = '';  // init current time
+    currentTime = currentTime.concat(String(today.getHours()), String(today.getMinutes()), String(today.getSeconds())); // hours:minutes:seconds to string
 
-  // get yesterday's date to clear out yesterday's data for next day's queue
-  if (currentTime.slice(0,2) == "23" && currentTime.slice(2,4) == "59" && "30" <= currentTime.slice(4,6) >= "59") {
-    var yesterday = today_date; // get date just before next day
-  }
+    // get yesterday's date to clear out yesterday's data for next day's queue
+    if (currentTime.slice(0,2) == "23" && currentTime.slice(2,4) == "59" && "30" <= currentTime.slice(4,6) >= "59") {
+      var yesterday = today_date; // get date just before next day
+    }
 
-  // At 12:00AM of next day, remove visited reports from yesterday
-  if (currentTime.slice(0,2) == "00" && currentTime.slice(2,4) == "00" && "00" <= currentTime.slice(4,6) >= "30") { // 15 second grace period
-    // empty visited queue of visited reports
-    for (var i = 0; i < visitedReports.length; i++) {
-      if (visitedReports[i].created_date.slice(0,10) == yesterday) {  // if report was from yesterday, remove it
-        delete visitedReports[i];
+    // At 12:00AM of next day, remove visited reports from yesterday
+    if (currentTime.slice(0,2) == "00" && currentTime.slice(2,4) == "00" && "00" <= currentTime.slice(4,6) >= "30") { // 15 second grace period
+      // empty visited queue of visited reports
+      for (var i = 0; i < visitedReports.length; i++) {
+        if (visitedReports[i].created_date.slice(0,10) == yesterday) {  // if report was from yesterday, remove it
+          delete visitedReports[i];
+        }
       }
     }
-  }
 
-  // Determine if report date is equal to today's date and not visited
-  // If not visited, add to visited and infoStore
-  // If visited, do not add to infoStore or visited and go to next iteration
-  for (var i = 0; i < (beforeStore.length - 10); i++) {
-    var report_date = beforeStore[17][i].created_date.slice(0,10);  // date formatting, 0's added for single digits already
-    if (report_date == today_date && !visitedReports.includes(beforeStore[17][i].created_date.slice(11,23))) {  // if report is today's date, and not posted already
-      infoStore.push(beforeStore[17][i]);  // push reports to info store
-      visitedReports.push(beforeStore[17][i].created_date.slice(11,23)); // push to visited because it was included in info store
-      // no two reports can have the same report time down to the millesecond
+    // Determine if report date is equal to today's date and not visited
+    // If not visited, add to visited and infoStore
+    // If visited, do not add to infoStore or visited and go to next iteration
+    for (var i = 0; i < (beforeStore.length - 10); i++) {
+      var report_date = beforeStore[17][i].created_date.slice(0,10);  // date formatting, 0's added for single digits already
+      if (report_date == today_date && !visitedReports.includes(beforeStore[17][i].created_date.slice(11,23))) {  // if report is today's date, and not posted already
+        infoStore.push(beforeStore[17][i]);  // push reports to info store
+        visitedReports.push(beforeStore[17][i].created_date.slice(11,23)); // push to visited because it was included in info store
+        // no two reports can have the same report time down to the millesecond
+      }
     }
-  }
 
-  if (infoStore === undefined || infoStore.length == 0) {  // if no reports, exit. 30 second refresh time in home page
-    console.log(trackNumber + " - " + "Nothing in infoStore, next iteration!");  // server logging purposes
+    if (infoStore === undefined || infoStore.length == 0) {  // if no reports, exit. 30 second refresh time in home page
+      console.log(trackNumber + " - " + "Nothing in infoStore, next iteration!");  // server logging purposes
+      trackNumber++;
+      sleep.sleep(10); // wait for new loop
+      continue;  // start next loop
+    }
+
+    // get JSON info on all reports and split up sections for status
+    for (let i = 0; i < infoStore.length; i++) {
+       var final_status = "";  // init our status
+
+       // add important data to our status with string concatenation
+       final_status = final_status.concat('On ', today_date, ' at ', (timeConverter(infoStore[i].created_date)).toString(), ' - ');
+       final_status = final_status.concat(today_date.replace(/-/g, '/'), ', an incident was reported by the NYPD as, ');
+       final_status = final_status.concat((infoStore[i].descriptor).toString(), '. Location: ', (infoStore[i].incident_address).toString(), '. ');
+       final_status = final_status.concat('The status of the incident is considered ', (infoStore[i].status).toString(), '.');
+
+       final_status = final_status.toString();
+
+       var final_tweet = { status: final_status }  // final_tweet struct
+
+       await sendTweets(final_tweet);  // await linked to the await given at the top of while loop
+       await sleep.sleep(4);
+    }
+
+    console.log(trackNumber+ " - " + "Finished tweet(s), next iteration!");  // server logging purposes
     trackNumber++;
-    sleep.sleep(10); // wait for new loop
-    continue;  // start next loop
-  }
+    sleep.sleep(5); // sleep for 10 seconds
 
-  // get JSON info on all reports and split up sections for status
-  for (var i = 0; i < infoStore.length; i++) {
-     var final_status = "";  // init our status
-
-     // add important data to our status with string concatenation
-     final_status = final_status.concat('Yesterday at ', (timeConverter(infoStore[i].created_date)).toString(), ' - ');
-     final_status = final_status.concat(today_date.replace(/-/g, '/'), ', an incident was reported by the NYPD as, ');
-     final_status = final_status.concat((infoStore[i].descriptor).toString(), '. Location: ', (infoStore[i].incident_address).toString(), '. ');
-     final_status = final_status.concat('The status of the incident is considered ', (infoStore[i].status).toString(), '.');
-
-     var final_tweet = {  // create tweet struct
-       status: final_status
-     }
-
-     console.log(final_status);  // server logging purposes
-
-     // Twitter npm module tweet function
-     client.post('statuses/update', final_tweet, function(error, tweet, response) {
-       if(error) {
-         console.log(error);
-       }
-       console.log(tweet);  // Tweet body.
-       console.log(response);  // Raw response object.
-     });
-  }
-
-  console.log(trackNumber+ " - " + "Finished tweet(s), next iteration!");  // server logging purposes
-  trackNumber++;
-  sleep.sleep(10); // sleep for 10 seconds
-
-} // end while loop
+  }  // End while loop      // Catch below needed for the async wrapper that wraps around the while(1) loop
+})().catch(e => { console.error(e) }) // Catch needed to prevent "Unhandled Promise Rejection" error
