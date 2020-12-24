47d23b1db709c9ab84b2eeb01a8a01f45616223b
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-12-23 21:00:17 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -79,6 +79,8 @@ var visitedReports = []; // store already posted reports, do this to prevent dup
 
 var trackNumber = 0; // Used to number logs
 
+console.log("");  // New line after terminal prompt
+
 // Async needed for while loop so sendTweets() has enough time and is not passed for not being immediate
 (async () => {
   // keep running indefinitely, server style
@@ -162,7 +164,7 @@ var trackNumber = 0; // Used to number logs
 
     console.log(trackNumber+ " - " + "Finished tweet(s), next iteration!");  // server logging purposes
     trackNumber++;
-    sleep.sleep(10); // sleep for 10 seconds
+    sleep.sleep(10); // sleep for 5 seconds
 
   }  // End while loop      // Catch below needed for the async wrapper that wraps around the while(1) loop
 })().catch(e => { console.error(e) }) // Catch needed to prevent "Unhandled Promise Rejection" error
